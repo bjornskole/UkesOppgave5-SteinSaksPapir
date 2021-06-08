557040f3f936a5557b4c1acb1ec4b79dc1ce3517
Bjorn Sveia <bjorn@radalabs.net>
2021-06-08 09:41:17 +0200

    update

--- controller.js
+++ controller.js
@@ -1,1 +1,31 @@
 // controller
+function gameEvaluator(strike) {
+  let randomNR = Math.floor(Math.random() * 3);
+  computerStrike = RSP[randomNR];
+  let index = condition(strike);
+  index === "Victory"
+    ? ((color = "green"), playerScore++)
+    : index === "Loss"
+    ? ((color = "red"), computerScore++)
+    : (color = "blue");
+  app();
+}
+function condition(strike, val) {
+  playerStrike = strike;
+  strike === computerStrike
+    ? (callback = "Draw")
+    : strike === "Paper" && computerStrike === "Rock"
+    ? ((callback = "you Win, Paper beats Rock"), (val = "Victory"))
+    : strike === "Rock" && computerStrike === "Paper"
+    ? ((callback = "You Lost, Paper beats Rock"), (val = "Loss"))
+    : strike === "Scissor" && computerStrike === "Paper"
+    ? ((callback = "You win, Scissors beats paper"), (val = "Victory"))
+    : strike === "Paper" && computerStrike === "Scissor"
+    ? ((callback = "You lost, Scissors beats Paper"), (val = "Loss"))
+    : strike === "Rock" && computerStrike === "Scissor"
+    ? ((callback = "You win, Rock beats Scissors"), (val = "Victory"))
+    : strike === "Scissor" && computerStrike === "Rock"
+    ? ((callback = "You lost, Rock beats Scissors"), (val = "Loss"))
+    : "null";
+  return val;
+}
--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 // model
+const RSP = ["Rock", "Scissor", "Paper"];
+// computer model
+let randomNR;
+let computerStrike = "";
+let playerStrike = "";
+let callback = "Throw a strike";
+let color = "black";
+let playerScore = 0;
+let computerScore = 0;
+
 // view
+
 app();
 function app() {
-  document.getElementById("app").innerHTML = `
-    <div>hello</div>
-    `;
+  document.getElementById("app").innerHTML = `<div id="page">
+  <div class="header">Rock Scissor Paper</div>
+  <div class="score" style="color: ${color}">${playerScore} - ${computerScore}<div>
+  <div class="computerStrike">${playerStrike}-${computerStrike} </div>
+  <div style="color:${color}" class="callback">${callback}</div>
+  <button value="Rock" id="btnOne" onclick="(gameEvaluator(this.value))">Rock</button>
+  <button value="Scissor" id="btnTwo" onclick="(gameEvaluator(this.value)">Scissor</button>
+  <button value="Paper" id="btnThree" onclick="(gameEvaluator(this.value)">Paper</button>
+  </div>`;
 }
-// controller
